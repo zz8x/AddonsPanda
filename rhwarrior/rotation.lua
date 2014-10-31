44ab2c0afdc5ebd3f30a3f2b7167ef49da0a7cce
kluory <kluory@gmail.com>
2014-10-31 23:49:22 +0400

    war1

--- rhwarrior/rotation.lua
+++ rhwarrior/rotation.lua
@@ -14,9 +14,9 @@ function Idle()
     -- дайте поесть (побегать) спокойно 
     if not IsAttack() and (IsMounted() or CanExitVehicle() or HasBuff(peaceBuff)) then return end
     
-    if IsCtr() or InCombatMode() then
+    --[[if IsCtr() or InCombatMode() then
        if TryHeal() then return end
-    end
+    end]]
 
 	if InCombatMode() then
         CheckTarget()
@@ -39,6 +39,25 @@ function Rotation()
     if TryInterrupt(TARGETS) then return end
  
     if IsNotAttack("target") then return end
+
+    if InRange("Рывок") and DoSpell("Рывок") then return end
+    if IsCtr() then
+        if DoSpell("Безрассудство") then return end
+        if UnitMana100(player) > 30 and DoSpell("Размашистые удары") then return end
+        if DoSpell("Знамя с черепом") then return end
+        if DoSpell("Вихрь клинков") then return end
+        return
+    end
+    --if not IsReadySpell("Рывок") and InRange("Рывок") and DoSpell("Героический прыжок", "target") then return end
+    if IsReadySpell("Победный раж") and UnitHealth100(player) < 70 and DoSpell("Победный раж")  then return end
+    if IsValidTarget() and UnitMana100(player) < 45 and DoSpell("Ярость берсерка") then return end
+    if InMelee(target) and DoSpell("Удар колосса") then return end
+    if InMelee(target) and UnitMana100(player) < 90 and DoSpell("Смертельный удар") then return end
+    if IsShift() and UnitMana100(player) > 30 and DoSpell("Вихрь") then return end
+    if InMelee(target) and UnitMana100(player) > 45 and DoSpell("Казнь") then return end
+    if InMelee(target) and UnitMana100(player) > 45 and DoSpell("Мощный удар") then return end
+    if InMelee(target) and DoSpell("Превосходство") then return end 
+    if DoSpell("Боевой крик") then return end
 
     --if DoSpell("Молния") then return end    
 end
