0021f6b4c2440d4352f00eb4ac75406b5de638c8
kluory <kluory@gmail.com>
2015-01-19 18:03:12 +0300

    fixiki

--- rhwarrior/rotation.lua
+++ rhwarrior/rotation.lua
@@ -2,7 +2,8 @@
 ------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------
 local peaceBuff = {"Пища", "Питье"}
-local slow = {"Пронзительный вой", "Подрезать сухожилия", "Ледяной шок", "Ледяные оковы", "Ледяная ловушка", "Ледная стрела", "Конус холода", "Замедление", "Вывести из строя", "Глаз быка", "Удар летящего змея", "Стрела ледяного огня", "Ледяная бомба", "Печать справедливости", "Пытка разума", "Калечащий яд", "Смертельный бросок", "Истощение", "Оковы земли", "Гром и молния", "Кровавая баня", "Вечная мерзлота", "Беспощадность зимы", "Головокружение", "Зараженные раны", "Тайфун", "Вихрь Урсола", "Контузящий выстрел", "Морозный след"}
+local burst = {"Волна побед", "Исступление","Безрассудство"}
+local evasion = {"Бой насмерть", "Ускользание"}
 
 function Idle()
     if IsAttack() or IsMouse(3) then
@@ -28,7 +29,7 @@ end
 
 function TryProtect()
     local hp = UnitHealth100("player")
-    --if IsPvP() and hp < 60 and DoSpell("Оборонительная стойка") then return end
+    --if IsPvP() and hp < 25 and DoSpell("Оборонительная стойка") then return end
     if hp < 50 and DoSpell("Бой насмерть") then return end
     if hp < 32 and DoSpell("Глухая оборона") then return end
     if HasBuff("Глухая оборона") and DoSpell("Ободряющий клич") then return end
@@ -48,7 +49,7 @@ function Rotation()
     if TryInterrupt(TARGETS) then return end
  
     if IsNotAttack("target") then return end
-    if IsPvP() and UnitHealth100("player") > 60 and DoSpell("Боевая стойка") then return end
+    if IsPvP() and UnitHealth100("player") > 40 and DoSpell("Боевая стойка") then return end
     
     if InRange("Рывок") and DoSpell("Рывок") then return end
 
@@ -61,24 +62,37 @@ function Rotation()
         return
     end
 
+    if HasBuff(evasion, 1, "target") then
+        print(1)
+        if InMelee(target) and DoSpell("Превосходство") then return end
+        if DoSpell("Смертельный удар") then return end
+    end
+
+    if HasBuff(burst) then
+        if InMelee(target) and not HasDebuff("Удар колосса", target) and DoSpell("Удар колосса") then return end
+        if InMelee(target) and DoSpell("Казнь") then return end
+        if InMelee(target) and DoSpell("Мощный удар") then return end
+        if DoSpell("Смертельный удар") then return end
+        if DoSpell("Боевой крик") then return end
+    end
+
     if IsReadySpell("Победный раж") and UnitHealth100(player) < 70 and DoSpell("Победный раж")  then return end
     
     if InMelee(target) and not HasDebuff("Удар колосса", target) and DoSpell("Удар колосса") then return end
 
-    if InMelee(target) and HasBuff("Безрассудство") and (not HasDebuff("Смертельные раны", 0.1, target) or (UnitMana100(player) < 30)) and DoSpell("Смертельный удар") then return end
-    if InMelee(target) and not HasBuff("Безрассудство") and DoSpell("Смертельный удар") then return end
+    --if InMelee(target) and HasBuff("Безрассудство") and (not HasDebuff("Смертельные раны", 0.1, target) or (UnitMana100(player) < 30)) and DoSpell("Смертельный удар") then return end
+    if InMelee(target) and DoSpell("Смертельный удар") then return end
     if InMelee(target) and DoSpell("Удар колосса") then return end
-    if not IsPvP() and IsValidTarget() and DoSpell("Удар громовержца") then return end
+    --if not IsPvP() and IsValidTarget() and DoSpell("Удар громовержца") then return end
 
     if IsShift() and UnitMana100(player) > 30 and DoSpell("Вихрь") then return end
 
-    if InMelee(target) and UnitMana100(player) > 45 and DoSpell("Казнь") then return end
-    if InMelee(target) and UnitMana100(player) > 40 and DoSpell("Мощный удар") then return end
-    if InMelee(target) and HasBuff("Безрассудство")--[[UnitMana100(player) > 45]] and DoSpell("Мощный удар") then return end
+    if InMelee(target) and UnitMana100(player) > 75 and DoSpell("Казнь") then return end
+    if InMelee(target) and UnitMana100(player) > 80 and DoSpell("Мощный удар") then return end
     if InMelee(target) and DoSpell("Превосходство") then return end
-    if InMelee(target) and DoSpell("Мощный удар") then return end
 
-    if IsValidTarget() and DoSpell("Героический бросок") then return end 
+    if IsValidTarget() and DoSpell("Героический бросок") then return end
+
     if not HasBuff("крик") and DoSpell("Боевой крик") then return end
     if HasBuff("крик") and not HasMyBuff("крик") then
         if HasBuff("Боевой крик") and DoSpell("Командирский крик") then return end
